91124f4cb1b9cf7a71db24d3e88726fb6def4d37
Dan Sciortino <disco@slac.stanford.edu>
2022-12-19 10:12:32 -0800

    adding pll-config folder and files

--- core/leap-core/ruckus.tcl
+++ core/leap-core/ruckus.tcl
@@ -3,3 +3,6 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 # Load local Source Code
 loadSource -lib epix_leap_core -dir "$::DIR_PATH/rtl"
+
+# Adding the default Si5345 configuration
+add_files -norecurse "$::DIR_PATH/pll-config/leapCorePllConfig.mem"
