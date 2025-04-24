775626067a66c84b63c2382edbbd5828bfba113f
Jenny Smith <jpsmith@slac.stanford.edu>
2025-04-24 15:33:23 -0700

    fix whitespace

--- python/epix_hr_core/_sDacRegisters.py
+++ python/epix_hr_core/_sDacRegisters.py
@@ -39,13 +39,13 @@ class sDacRegisters(pr.Device):
         
         # Add Dummy Register
         self.add(pr.RemoteVariable(
-        name = 'dummy',         
-        description = '',                  
-        offset = 0x00014, 
-        bitSize = 32,   
-        bitOffset = 0,   
-        base = pr.UInt, 
-        disp = '{:#x}', 
+        name = 'dummy',
+        description = '',
+        offset = 0x00014,
+        bitSize = 32,
+        bitOffset = 0,
+        base = pr.UInt,
+        disp = '{:#x}',
         mode = 'RW'))
 
         # Add DAC Voltage Linker Variables
